7b63bff54bb1327c1f105765e29d4dbf19ce38c3
root <root@slave.tp-tw>
2018-05-25 00:50:20 -0700

    modified ftp.sh

--- ftp.sh
+++ ftp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-HOST=199.101.116.38
-USER=rbbackup
-PASSWORD=DJFLDJFSF
+HOST=xxx.xxx.xxx.xxx
+USER=xx
+PASSWORD=xxx
 ftp   -i -u $HOST <<EOF
 user $USER $PASSWORD
 binary
-cd  test
-put a.sh
-get tom.sh
+cd    direcotory
+put file1
+get file2
 quit
 EOF
 
